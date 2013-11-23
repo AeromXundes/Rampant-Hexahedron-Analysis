0e0e60902533de0690f0df6a5079472c46daef18
AeromXundes <aerom.xundes@gmail.com>
2013-11-23 07:34:25 -0500

    Added some classes preparing for aggregate stats

---
+++ RHA.Analyzers.ClusterStatistics/AggregateClusterStats.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RHA.Analyzers.DataPoints;
+
+namespace ClusterStatistics
+{
+    public class AggregateClusterStats
+    {
+        double BlocksPerClusterAvg;
+        double ClustersPerChunkAvg;
+        double BlocksPerChunkAvg;
+        List<int> CentroidHeatMapChunkXZPlane;
+        List<int> CentroidHeatMapChunkXYPlane;
+        List<int> CentroidHeatMapChunkYZPlane;
+        double ClusterYLengthAvg;
+        double ClusterXLengthAvg;
+        double ClusterZLengthAvg;
+
+        List<ClusterDataPoint> Clusters;
+        Dictionary<Tuple<int, int>, bool> ChunkMap;
+        Dictionary<Tuple<int, int>, Biome> BiomeMap;
+    }
+}
---
+++ RHA.Analyzers/DataPoints/Biome.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RHA.Analyzers.DataPoints
+{
+    public class Biome
+    {
+        int Id;
+        String Name;
+        float Temperature;
+    }
+}
